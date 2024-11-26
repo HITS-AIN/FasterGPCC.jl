193634dda6f1cc21c2d970e8e59844ae798a0f8b
Nikos Gianniotis <ngiann@gmail.com>
2024-11-26 15:31:30 +0100

    first code commit

---
+++ src/FasterGPCC.jl
@@ -0,0 +1,15 @@
+module FasterGPCC
+
+    using GPCC
+
+    using ProgressMeter, ThreadTools, Printf
+
+    using Memoization, ThreadSafeDicts
+
+    # Following line makes ProgressMeter work with tmap1
+
+    ProgressMeter.ncalls(::typeof(tmap1), ::Function, args...) = ProgressMeter.ncalls_map(args...)
+
+    include("posteriordelay.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FasterGPCC
+using Test
+
+@testset "FasterGPCC.jl" begin
+    # Write your tests here.
+end
